4e371899516fe3d998005b0a909908efe66394b3
dharmendra <dharmendrasinghyadav022@gmail.com>
2024-04-12 13:49:34 +0530

    updated-code

--- src/components/Home.js
+++ src/components/Home.js
@@ -191,7 +191,7 @@ const Home = () => {
                                 <p>
                                     randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All randomised words which don't look even slightly
                                 </p>
-                                <a >
+                                <a href="/home">
                                     Read More
                                 </a>
                             </div>
